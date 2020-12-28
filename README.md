f829672e3bd61f30fbf52caa6d625c84f5a45ba6
frankenstynsh <guptaayush782@outlook.com>
2020-12-28 08:54:43 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# yelp-india
-yelp camp website aiming to track popular campgrounds in india by user added preferences
+# Yelp-camp
+yelp camp modified website under construction.
+Its a mod website of a popular tourist website called yelp camp which allows user to browse through various campgrounds spread  across the country and book their visits. 
+It even allows users to add their own new campgrounds to facilitate others.
+
+to run the website on the local server file named app.js nned to be run.
---
+++ app.js
@@ -0,0 +1,79 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var request = require("request");
+var mongoose = require("mongoose");
+
+const connurl = "mongodb+srv://admin:qwerty123@cluster0.pn1m3.mongodb.net/yelp?retryWrites=true&w=majority";
+mongoose.connect(connurl,{
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+// schema
+var campgroundschema = new mongoose.Schema({
+    name : String,
+    image : String,
+    description : String
+});
+var campground= mongoose.model("campground" , campgroundschema);
+
+// campground.create({
+//     name : "Solang Valley",
+//     image : "https://i.imgur.com/vqEsn6o.jpg?1"
+// },function (err,campground){ 
+//     if(err){
+//         console.log(err);
+//     } else {
+//         console.log("okok");
+//         console.log(campground);
+//     }
+// });
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.set("view engine","ejs");
+// var campgrounds=[
+//     {name :"Spiti Valley" ,image :"https://i.imgur.com/vqEsn6o.jpg?1"},
+//     {name :"Chandratal lake", image :"https://i.imgur.com/G5dU3Zx.jpg"},
+//     {name :"Solang Valley" ,image :"https://i.imgur.com/FDhzVukg.jpg"}
+// ];
+app.get("/",function(req,res){
+    res.render("landing");
+});
+app.get("/campgrounds",function(req,res){
+    //from db 
+    campground.find({},function(err, allcampgrounds){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("campgrounds",{campgrounds:allcampgrounds});
+        }
+    })
+});
+
+app.post("/campgrounds",function(req,res){
+    // var name =req.body.name;
+    // var image = req.body.image;
+    var newCampground ={
+        name: req.body.name,
+        image: req.body.image,
+        description: req.body.description}
+
+    campground.create(newCampground, function(err, newlycreated){
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect("/campgrounds");
+        }
+    });
+    // campgrounds.push(newCampground);
+    // res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new",function(req,res){
+    res.render("new");
+});
+
+app.listen(3000,function(){
+    console.log("server is listening");
+});
